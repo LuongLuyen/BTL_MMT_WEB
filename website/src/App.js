e14578988a7f8729195e3755082c2c9cddb24647
LuongLuyen <luongvanluyen2003@gmail.com>
2023-09-26 14:05:05 +0700

    send message

--- website/src/App.js
+++ website/src/App.js
@@ -1,29 +1,52 @@
-import './App.css';
-import React, { useState, useEffect, useRef } from "react"
+import React, {useEffect, useRef,useState } from "react"
 import socketIOClient from "socket.io-client"
-
 function App() {
+  const [data, setData] = useState([])
+  const [name, setName] = useState('')
+  const [input, setInput] = useState('')
   const socketRef = useRef()
+  
   useEffect(() => {
     socketRef.current = socketIOClient.connect("http://localhost:5000")
-    socketRef.current.on('ServerToClient', data => {
-      console.log(data)
-    })
     return () => {
       socketRef.current.disconnect()
     }
   }, [])
-  const send = () => {
-    const data = {
-      id:1,
-      content: "client"
-    }
-    socketRef.current.emit('ClientToServer', data)
+  const Login = ()=>{
+    const name = prompt("Nhập tên ?")
+    setName(name)
   }
+  const send = () => {
+    if(name === ""){
+      Login()
+    }else{
+      socketRef.current.emit('ClientToServer',{
+        name:name,
+        content: input
+      })
+      socketRef.current.on('ServerToClient', data => {
+        setData(data)
+      })
+    }
+  }
+
   return (
-    <div className="App">
+    <div className="Main">
       <h1>Client BTL Mạng máy tính</h1>
-      <div onClick={send}>btn</div>
+      <div>Đây là: {name}</div>
+            <div>
+              {data.map((item,index)=>(
+                <div key={index}>
+                  <span>Name:  {item.name}</span>
+                  <br/>
+                  <span>Message:   {item.content}</span>
+                  <br/>
+                  <br/>
+                </div>
+              ))}
+            </div>
+          <input value={input} onChange={(e)=>{setInput(e.target.value)}}/>
+          <div onClick={send}>GỬI</div>
     </div>
   );
 }
